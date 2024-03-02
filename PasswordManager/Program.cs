55c74121200c1096dc4cb5006c3d4dc83062b3e1
Hanna Holm <hanna.holm@hotmail.com>
2024-03-02 12:32:29 +0100

    Added intergation tests project

--- PasswordManager/Program.cs
+++ PasswordManager/Program.cs
@@ -4,9 +4,9 @@ using System.Text;
 
 namespace PasswordManager
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             string command = args[0].ToLower();
 
